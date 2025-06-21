6e6bf925d3e0f082b1adc34461100730d3f8ad72
rjmchi <robert@rjmprogramming.com>
2025-06-21 11:26:35 -0600

    changes vite base back

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    base: 'https://laravel-production-26ad.up.railway.app/',
+    // base: 'https://laravel-production-26ad.up.railway.app/',
+    base: "./",
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
